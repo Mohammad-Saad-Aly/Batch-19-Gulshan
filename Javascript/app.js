adcd7608dfd9f870540fe440f6bceb8747675756
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-12-26 12:50:49 +0500

    Completed JS Chapters 1–5 (Smarter Way to Learn JavaScript)

---
+++ Javascript/app.js
@@ -0,0 +1,29 @@
+// document.writeln("<h1> Hello Saad </h1> <br>")
+// document.writeln("Hello World")
+
+// document.writeln("hello" + " " + "world")
+
+// document.writeln("hello " + "saad")
+
+// alert(" Welcome \n User ")
+
+// document.writeln("1" + 1 + 1)
+
+// document.writeln("10" * "abc")
+
+// 10abc
+// 10abc 10abc 10abc 10abc 10abc 10abc 10abc 10abc
+// NaN
+// error
+
+// document.writeln(10 - 5)
+
+
+// var firstName = "Saad"
+// document.writeln(firstName)
+
+// var first_name = 5
+// var num2 = 10
+// document.writeln(num + num2)
+
+// document.writeln(3 % 2)
