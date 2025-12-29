d42d1c60231a67107410f148625eba74fba021df
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-12-29 12:50:21 +0500

    Completed JS Chapters 5–6 (Smarter Way to Learn JavaScript)

---
+++ Javascript/class 2/app.js
@@ -0,0 +1,58 @@
+// document.writeln("Hello world <br>")
+
+// alert("Hello Saad")
+
+// var user = "Mohammad Saad Ali"
+
+// document.writeln(user)
+
+// var num = "10"
+// var num2 = "30"
+// document.writeln(num - num2)
+
+// 300 xyz
+// 1030 xyz
+// error
+// NaN
+
+
+// post increment
+// pre increment
+// post decrement
+// pre decrement
+
+
+// var a = 2
+// var b = 3
+
+// var c = ++a + ++a + ++b + ++b
+
+// document.writeln(c)
+
+// var a = 5
+// var c = a++ + a++ + a++ + a
+// document.writeln(a)
+
+
+// var a = 2
+// var b = 3
+// var c = ++b + ++a + a++ + b++ + a++ + ++a + b + b++
+//         //  4  +  3  +  3  +  4  +  4  +  6  + 5 +  5
+// document.writeln(c)
+
+// var a = 5
+// var b = 7
+// var c = 10
+// var d = a++ + ++a + b++ + --b + --c + a-- - --a + --b + --c + --a + c++ - b-- - a-- - b-- + c-- + b + c + c + a + b
+// //  5  +  7  +  7  +  7  +  9  +  7  -  5  +  6  +  8  +  4  +  8  -  6  -  4  -  5  +  9  + 4 + 8 + 8 + 3 + 4
+// document.writeln(d)
+
+
+
+var a = 2
+var b = 3
+var c = 4
+var d = a++ + ++a - --b - b++ - c++ + 6 - c++ - a++ - 7 - a++ + ++a + b-- + --b + a++ - --c - c-- + a++ + a-- + b-- - 10
+        //  2  +  4  -  2  -  2  -  4  + 6 -  5  -  4  - 7 -  5  +  7  +  3  +  1  +  7  -  5  - 5   +  8  +  9  +  1  - 10
+
+        document.writeln(d)
