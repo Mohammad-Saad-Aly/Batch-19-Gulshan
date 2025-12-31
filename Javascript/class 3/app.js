715da6ba71728c37d6a3a41378d028648f7a5267
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-12-31 12:45:22 +0500

    Completed JS Chapters 6–9 (Smarter Way to Learn JavaScript)

---
+++ Javascript/class 3/app.js
@@ -0,0 +1,73 @@
+// var a = 3
+// a -= 14
+// a = a + 4
+// document.writeln(a)
+
+
+// DMAS
+// BODMAS
+
+// var eq = 5 * 3 + 3 - (7 * 3 + 2 + (8 * 2) + 10) + 4 * 5
+// document.writeln(eq)
+
+// var user = "Saad Ali"
+// var email = "saad@gmail.com"
+
+// document.writeln(user + " " + email)
+
+// var msg = "Enter Your Name"
+// var user = prompt(msg, "John")
+// document.writeln(user)
+
+// template literals ( concat ka alternate ) + + + + -------------- ${}
+
+// var num1 = +prompt("Enter value 1")
+// var num2 = +prompt("Enter value 2")
+
+// var add = num1 + num2
+// var sub = num1 - num2
+// var mul = num1 * num2
+// var div = num1 / num2
+// var mod = num1 % num2
+
+
+// document.writeln(
+//     `
+//     The addition of ${num1} and ${num2} = ${add} <br> 
+//     The subtraction of ${num1} and ${num2} = ${sub} <br> 
+//     The Multiplication of ${num1} and ${num2} = ${mul} <br> 
+//     The Division of ${num1} and ${num2} = ${div} <br> 
+//     The Modulus of ${num1} and ${num2} = ${mod} <br> 
+//     `
+// )
+
+// var firstName = "Mohammad"
+// var lastName = "Saad Ali"
+// document.writeln(`${firstName} <br> ${lastName}`)
+
+// document.writeln(firstName + " " + lastName)
+
+// var num1 = prompt("Enter value 1")
+// var num2 = prompt("Enter value 2")
+
+// document.writeln(typeof(+prompt()))
+// document.writeln(typeof(parseInt(prompt())))
+// document.writeln(typeof(Number(prompt())))
+
+// var a = +prompt("Value 1")
+// var b = +prompt("Value 2")
+
+// document.writeln(a + b)
+
+
+// Chapter 1 ( Alert )
+// Ans no 1
+// alert("Welcome Habibi")
+
+// Ans no 2
+// alert("Error! Please Enter Valid Password")
+
+// Chapter 1 end 
+
+// -------------------------------------------------------------------------
+
