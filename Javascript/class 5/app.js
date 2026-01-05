d8475d6b4ac2ebc58ee7c7c1da4fe07c3ce77de7
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-01-05 12:32:13 +0500

    Completed JS Chapters 13–14 (Smarter Way to Learn JavaScript)

---
+++ Javascript/class 5/app.js
@@ -0,0 +1,72 @@
+// var a = 5
+// var b = 5
+// document.writeln(a === b)
+
+// > greater
+// < lesser
+// >= greater or equal
+// <= lesser or equal
+// != not equal 
+// !== strictly not equal
+// == value check
+// === value or data type both check hai
+
+// var user = "admin"
+// var password = "123456"
+
+// if( user == "admin" && password == "123" ){
+//     alert("Welcome Admin")
+// }
+// else {
+//     alert("Invalid credentials")
+// }
+
+// document.writeln(5 < 3 || 8 == 8)
+
+
+// var user = prompt("enter your username")
+// var password = prompt("enter your password")
+
+// if(user == "saad" || password == "ali"){
+//     document.writeln("Login Success")
+// }
+// else {
+//     document.writeln("Invalid Credentials")
+// }
+
+// var a = 10
+// var b = 5
+
+// if (a == 10){
+//     document.writeln("a to sahi gaya lekin <br>")
+//     if (b == 15){
+//         document.writeln("a aur b dono sahi hai")
+//     }
+//     else {
+//         document.writeln("b galat hogaya")
+//     }
+// }
+// else {
+//     document.writeln("a galat hogaya")
+// }
+
+
+// falsy values in JS
+// false
+// 0
+// null
+// undefined
+// ''
+// NaN
+
+// except these values all values are true
+// var eq = 5 == 5 && "undefined" && "false" && "bhatook"
+// document.writeln(eq)
+
+
+var eq =   "ansari"
+
+document.writeln(eq)
+
+
+
