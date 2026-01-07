598c034feebafde9a20afd42b2f8bce8fe03e163
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-01-07 12:44:03 +0500

    Completed JS Chapters 15–16 (Smarter Way to Learn JavaScript)

---
+++ Javascript/class 6/app.js
@@ -0,0 +1,64 @@
+// var studentName = "Abdul Rehman"
+// var age = 25
+// var email = "abrehman@gmail.com"
+// var batch = "Batch 19"
+// document.writeln(studentName, "<br>" + age + "<br>", email + "<br>", batch)
+
+// Array
+// coollection of Data ( in JS )
+
+// var student = ["Saad", 26, "saad@gmail.com", "Batch 19"]
+// document.writeln(student[1000])
+// document.writeln(student[1])
+// document.writeln(student[0] , " " ,student[3])
+
+// const test = "test123";
+// test = "Saad"
+
+// test = "Saylani"
+// test = "SMIT"
+// document.writeln(test)
+
+
+// var fruits = ["nashpati", 'kinno', 'angoor', 'aaro', 'kaila', 'Aaam']
+
+// fruits[6] = "Tarboooooooooz"
+// fruits[4] = "Santraayyy"
+
+// document.writeln(fruits[0] + "<br>")
+// document.writeln(fruits[1] + "<br>")
+// document.writeln(fruits[2] + "<br>")
+// document.writeln(fruits[3] + "<br>")
+// document.writeln(fruits[4] + "<br>")
+// document.writeln(fruits[5] + "<br>")
+// document.writeln(fruits[6] + "<br>")
+// document.writeln(fruits[1], " " ,fruits[5])
+
+
+
+
+// var a = sabziyan.pop()
+// var b = sabziyan.pop()
+// var c = sabziyan.pop()
+// document.writeln(a, b, c)
+
+
+
+
+// sabziyan.push("Lookey", "Mooli", "Gaajar")
+
+
+// push (akhir se add karta hai)
+// pop (akhir se delete karta hai)
+// unshift (shru se add karta)
+// shift (shru se delete karta hai)
+
+
+var sabziyan = ["Aloo", "timatar", "lassan", "bhindi", "Matar"] 
+sabziyan[1] = "amir Ansari"
+
+document.writeln(sabziyan)
+
+// sabziyan.unshift("Mirch", "Lemo")
+// var ab = sabziyan.shift()
+
