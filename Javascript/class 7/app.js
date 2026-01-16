c398c855bdda6bb17ae4f0a9a1c6273fae618dcb
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-01-16 12:36:49 +0500

    17 to 19

---
+++ Javascript/class 7/app.js
@@ -0,0 +1,71 @@
+// {1,2,3 ........ n}
+
+// var students = ["Ahmed", "Asad", "Saad", "Taha", "sumair", "babar"]
+
+// var newArray = students.slice(2, 5)
+
+// document.writeln(newArray)
+
+
+// var fruits = [ 'apple', 'banana', 'orange', 'kiwi']
+// document.writeln(copiedArray)
+// slice n - 1
+// slice(index, length)
+// var copiedArray = fruits.slice(0, 3)
+
+// fruits.splice(3, 0, "abcd", "xyz")
+// fruits.splice(1, 2, "xyz")
+// fruits.splice(0, 0, "abcd")
+// fruits.shift()
+// fruits.shift()
+// fruits.shift()
+
+// fruits.unshift("abcd ", "xyz")
+// fruits.pop()
+// fruits.pop()
+
+// push
+// pop
+// unshift
+// shift
+// splice
+// slice
+
+
+// fruits[0] = 'strawberry'
+// document.writeln(fruits)
+// document.writeln(fruits[2], fruits[1])
+
+//         // -4    -3    -2    -1 
+// var arr = ["a", "b", "c", "d"]
+//         //  0    1    2    3
+
+// var newArr = arr.slice(2, -1)
+
+// document.writeln(newArr)
+
+
+// var students = [];
+// var students = new Array()
+
+// var str = ["a", "b", "c", "d"]
+// var nums = [1, 2, 3, 4, 5]
+// var bool = [true, false, true, false]
+// var mixedArray = [true, 0, "Saad", 20.5, null, undefined, NaN]
+// document.writeln(mixedArray)
+
+
+
+// var edu_qualifications = ["SSC", "HSC", "BSC"]
+
+// document.writeln(
+//     `
+//     <h1> Qualifications </h1>
+//     <br>
+//     0) ${edu_qualifications[0]} <br> 
+//     1) ${edu_qualifications[1]} <br>
+//     2) ${edu_qualifications[2]} <br>
+
+
+//     `
+// )
---
+++ Javascript/class 8/app.js
@@ -0,0 +1,36 @@
+// loop ( iteration baar baar koi kaam krna ya karwana )
+
+// for loop
+// while loop
+// do while loop
+
+
+// for(initialization, condition, inc/dec){
+// }
+
+// for(var i=1; i <= 10; i++){
+//     document.writeln(i + "<br>")
+// }
+
+// for(var i=1; i <= 10; i++){
+//     document.writeln(i+ ") Saad <br>")
+// }
+
+
+// for(var i=1; i<=100; i++){
+//     document.writeln(i + " <hr>")
+// }
+
+// for(var i=1; i<=10; i++){
+//     document.writeln(`570 x ${i} = ${570 * i}  <br>`)
+// }
+
+// var userNum = prompt("Enter your table number")
+
+// for(var i=1; i <= 10; i++){
+//     document.writeln(`${userNum} x ${i} = ${userNum * i}   <br>`)
+// }
+
+for (var i=1 ; i <= 10; i = i +2 ){
+    document.writeln(i + "<br>")
+}
