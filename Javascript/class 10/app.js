bbfd14f019f56860d035b5fcd690b6d4e64b88ad
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-01-21 12:33:27 +0500

    for loops

---
+++ Javascript/class 10/app.js
@@ -0,0 +1,54 @@
+// for (var i = 1; i <= 10; i++) {
+//     document.writeln(i + "<br>")
+// }
+
+// for(var i=10; i >= 1; i--){
+//     document.writeln(i + "<br>")
+// }
+
+// for (var i = -1; i >= -10; i--) {
+//     document.writeln(i + "<br>")
+// }
+
+// var i = 10
+// i = i - 5
+// console.log(i)
+
+
+// 1, 3, 5, 7, 9
+// for (var i = 0; i <= 10; i = i + 2) {
+//     console.log(i)
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//     if(i % 2 == 1){
+//         console.log("number is odd "+ i)
+//     }
+//     else {
+//         console.log("number is even "+ i)
+//     }
+// }
+
+// Infinite Loop
+// for (var i = 1; i > 0; i++) {
+//     console.log(i)
+// }
+
+
+// var fruits = ["apple", "banana", "orange", "amrood" ,"mango", "kiwi"]
+
+// for(var i=0; i < fruits.length; i++){
+//     document.writeln(fruits[i] + "<br>")
+// }
+
+
+var multiDimensional = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11]
+]
+
+
+for(var i=0; i < multiDimensional.length; i++){
+    document.writeln(multiDimensional[i] + "<br>")
+}
