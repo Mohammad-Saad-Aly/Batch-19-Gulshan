d381edeab5ca388776a887c5b37d914e458ff8eb
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-01-23 12:37:43 +0500

    23 jan 2026

---
+++ Javascript/class 11/app.js
@@ -0,0 +1,70 @@
+// 'mehran' ==  'altomehrancivicjacrevofortuner'
+
+// var cars = ['alto', 'mehran', 'civic', 'jac', 'revo', 'fortuner']
+// var user = prompt("Enter your desired car")
+// var match = 'saad'
+
+// for(var i=0; i < cars.length; i++){
+
+//     if(user == cars[i]){
+//         match = 'sadia'
+//         document.writeln(user + " car available")
+//     }
+
+// }
+
+// if(match == 'saad'){
+//     document.writeln(user + " car not available")
+// }
+
+
+// indexOf        == shru se read karega
+// lastIndexOf    == last se read karega
+
+
+// var a = ['cookie', 'patties', 'cake', 'apple pie', 'chips', 'patties']
+// var user = prompt('enter your item')
+// var match = false
+
+// for(var i=0; i < a.length; i++){
+//     if(user == a[i]){
+//         match = true
+//         document.writeln(user + " item found at index Number " + a.indexOf(user) )
+//         break
+//     }
+// }
+
+// if(match == false){
+//     document.writeln(user + ' item not found')
+// }
+
+
+// for(var i=1; i<=10; i++){
+//     if(i == 5){
+//         continue
+//     }
+//     document.writeln(i + "<br>")
+// }
+
+
+// a[0] > a[i]
+
+
+// var nums = [10, 70, 40, 500, 30]
+
+// var myNum = nums[0] // 12
+
+// for(var i=0; i < nums.length; i++){
+//     if(nums[i] < myNum){
+//         myNum = nums[i]
+//     }
+// }
+
+// document.writeln(myNum)
+
+
+// palindrome
+// madam, 121, mam, wow, MQM, civic, PPP, MOM, Dad
+
+// saad == daas ...... its not palindrome
+// 121  == 121  ...... its palindrome
