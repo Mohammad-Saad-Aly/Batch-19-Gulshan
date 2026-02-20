aa41c3b7bc7a4c8d63d7cd9895a799ac5db99981
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-02-20 08:30:22 +0500

    20 feb 2026

--- Javascript/class 22/app.js
+++ Javascript/class 22/app.js
@@ -26,9 +26,49 @@
 
 var getList = document.getElementById('list')
 
-function addTask(){
+function addTask() {
     var getInput = document.getElementById('form12')
-    getList.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center"> <span> ${getInput.value} </span> <span> <button type="button" class="btn btn-outline-info" data-mdb-ripple-init data-mdb-ripple-color="dark"> Edit </button> <button type="button" class="btn btn-outline-danger" data-mdb-ripple-init data-mdb-ripple-color="dark"> Delete </button> </span>  </li>`
-    getInput.value = ''
+
+    if (getInput.value == '') {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Please enter some value!"
+        });
+    }
+    else {
+        getList.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center"> <span> ${getInput.value} </span> <span> <button onclick='editList(this)' type="button" class="btn btn-outline-info" data-mdb-ripple-init data-mdb-ripple-color="dark"> Edit </button> <button onclick='delList(this)' type="button" class="btn btn-outline-danger" data-mdb-ripple-init data-mdb-ripple-color="dark"> Delete </button> </span>  </li>`
+        getInput.value = ''
+    }
+
+
 }
 
+
+function delList(item) {
+
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+            });
+            item.parentNode.parentNode.remove()
+        }
+    });
+}
+
+
+function editList(item) {
+    var updatedValue = prompt("Enter updated value", item.parentNode.parentNode.childNodes[1].textContent)
+    item.parentNode.parentNode.childNodes[1].textContent = updatedValue
+}
