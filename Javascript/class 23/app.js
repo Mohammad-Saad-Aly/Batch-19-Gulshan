106f90a4d938642b5ee0fa3048a21bfacc933ab7
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-02-23 08:15:39 +0500

    23 feb 2026

---
+++ Javascript/class 23/app.js
@@ -0,0 +1,38 @@
+// var student = {
+//     stud_id: 1,
+//     stud_name: "Hamza",
+//     stud_rollNo: "12345",
+//     stud_contact: 12345678910
+// }
+
+// document.writeln(student.stud_contact)
+
+// var test = {
+//     id: 1,
+//     name1: "Asad",
+//     age: 23.4,
+//     bool: true,
+//     check: undefined,
+//     activities: ["cricket", "swimming", "programming"],
+//     foo: function(){
+//         return this.name1 + this.age
+//     },
+//     student: {
+//         stud_id: '02',
+//         stud_name: "Wasay"
+//     }
+// }
+// var getValue = test.foo()
+
+// console.log(getValue)
+
+// for(var i=0; i < test.activities.length; i++){
+//     document.writeln(test.activities[i] + "<br>")
+// }
+
+// var amirAnsari = new Object()
+
+// amirAnsari.id = 1
+// amirAnsari.arr = ["hello", "world"]
+
+// console.log(amirAnsari)
