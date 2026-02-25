9cb5989c76d34d72e271adaf841688b3e567270c
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2026-02-25 08:17:40 +0500

    25 feb 2026

---
+++ Javascript/class 24/app.js
@@ -0,0 +1,54 @@
+// var obj = {
+//     id: "01",
+//     first_name: "Mohammad Saad",
+//     last_name: "Ali",
+//     age: 26,
+//     "organization": "SMIT"
+// }
+
+// if('class' in obj){
+//     document.writeln("han class hai")
+// }
+// else {
+//     obj.class = "main"
+//     console.log(obj)
+// }
+
+
+// document.writeln('class' in obj)
+
+// for (var key in obj) {
+//     document.writeln(key + " : " + obj[key] + "<br>")
+// }
+
+// document.writeln(obj['last_name'])
+// document.writeln(obj.first_name)
+
+
+// var cars = {
+//     alto: {
+//         id: 1,
+//         name: "alto",
+//         make: "2015",
+//         color: "white"
+//     },
+//     civic: {
+//         id: 2,
+//         name: "civic",
+//         make: "2023",
+//         color: "black"
+//     }
+// }
+
+// for(var key in cars.alto, cars.civic){
+//     document.writeln(cars.alto[key] + " --- " +cars.civic[key] + "<br>")
+// }
+
+
+
+
+
+
+
+
+
